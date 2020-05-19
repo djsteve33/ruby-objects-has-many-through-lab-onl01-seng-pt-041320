a19bc10955f0abd89f5615ddeacf58b18409becb
Steve Womble <swomble@aol.com>
2020-05-19 16:23:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,8 +16,8 @@ class Artist
     Song.all.select {|songs| songs.artist == self}
   end
   
-  def new_song(title, genre)
-    Song.new(title, self, genre)
+  def new_song(name, genre)
+    Song.new(name, self, genre)
   end
   
   def genres
