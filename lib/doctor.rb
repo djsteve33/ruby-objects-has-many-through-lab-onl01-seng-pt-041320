ded5304e61a2ece5e4d2690fdfc662c0c1522b5a
Steve Womble <swomble@aol.com>
2020-05-19 16:06:02 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,7 +17,7 @@ class Doctor
   end
   
   def appointments
-    Appointment.all.select do |appointment| appointment.patient == self
+    Appointment.all.select {|appointment| appointment.patient == self}
   end
   
   def patients
